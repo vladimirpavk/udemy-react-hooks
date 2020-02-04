ee280dafe953b3b99a8bed9d05626154525755c5
Vladimir Pavković <vladimirpavk@gmail.com>
2020-02-04 13:44:11 +0100

    +

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  const [inputState, setInputState] = useState(
+    {
+      title: '',
+      amount: ''
+    }
+  );
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
+    console.log(inputState.title, inputState.amount);
   };
+
+  const titleChangedHandler = (event)=>{
+    event.persist();
+    setInputState(
+      (previousState)=>{
+        return  {
+          title: event.target.value,
+          amount: previousState.amount
+        }
+      }     
+    );    
+  }
+
+  const amountChangedHandler = (event)=>{
+    event.persist();
+    setInputState(
+      (previousState)=>{
+        return {
+          title: previousState.title,
+          amount: event.target.value
+        }
+      }
+    );
+  }
 
   return (
     <section className="ingredient-form">
@@ -15,11 +47,11 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={inputState.title} onChange={titleChangedHandler} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={inputState.amount} onChange={amountChangedHandler} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
